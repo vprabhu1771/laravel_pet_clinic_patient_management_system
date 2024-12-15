bb6f04158b5be16ab9d3713c3aeeca3b17891e73
Prabhu <vprabhu1771@gmail.com>
2024-12-15 20:16:23 +0530

    Owner model and migration
    php artisan make:model Owner -m

---
+++ app/Models/Owner.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Owner extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'gender',
+        'email',
+        'phone',
+    ];
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
+}
