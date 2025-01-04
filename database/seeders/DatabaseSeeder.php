d9c9e21b379cbfa8151282a07993d4eb9866e8bf
Prabhu <vprabhu1771@gmail.com>
2025-01-04 17:15:40 +0530

    PatientSeeder added to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call([
-            OwnerSeeder::class
+            OwnerSeeder::class,
+            PatientSeeder::class
         ]);
     }
 }
